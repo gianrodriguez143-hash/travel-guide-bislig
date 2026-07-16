02c4abe2c146e983be906edf03d3fed602107249
gianrodriguez143-hash <gianrodriguez143@gmail.com>
2026-07-16 12:54:26 +0800

    Add files via upload

---
+++ bislig/register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="register.css">
+</head>
+<body>
+    <a href="login.php" class="primary-btn">Back</a>
+    
+    <form action="register_handler.php" method="POST">
+
+        <div class="mb-3">
+            <label class="form-label">Full Name</label>
+            <input
+                type="text"
+                class="form-control"
+                name="full_name"
+                required>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Email Address</label>
+            <input
+                type="email"
+                class="form-control"
+                name="email"
+                required>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input
+                type="password"
+                class="form-control"
+                name="password"
+                required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+            Register
+        </button>
+
+    </form> 
+</body>
+</html>
+ 
